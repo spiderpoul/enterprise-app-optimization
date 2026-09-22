2e7cc7a017349b76a1638f9f8c7ea6321d1fef1a
Pavel Uvarov <spiderpoul@mail.ru>
2026-09-22 12:47:37 +0300

    presentation: polish flow, notes and navigation

---
+++ presentation/global-bottom.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="slide-footer" aria-hidden="true">
+    <span class="slide-page">{{ $slidev.nav.currentPage }}</span>
+    <span class="slide-progress-track">
+      <span
+        class="slide-progress-bar"
+        :style="{ width: `${($slidev.nav.currentPage / $slidev.nav.total) * 100}%` }"
+      />
+    </span>
+  </footer>
+</template>
